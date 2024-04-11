6d5185d088e6eba7883c8f1dce9575f5e2df5c38
akashes <139908260+akashes@users.noreply.github.com>
2024-04-11 09:29:01 +0530

    Create errorHandlerMiddleware.js

---
+++ errorHandlerMiddleware.js
@@ -0,0 +1,15 @@
+//use this custom middelware at the botton just before listening to the server  app.use(errorHandler)
+const {logEvents} = require('./logger')
+
+const errorHandler=(err,req,res,next)=>{
+    logEvents(`${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errLog.log')
+    console.log(err.stack)
+
+    const status = res.statusCode ? res.statusCode : 500 // server error 
+
+    res.status(status)
+
+    res.json({ message: err.message })
+}
+
+module.exports = errorHandler
